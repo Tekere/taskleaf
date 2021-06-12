006abecf61c64230c4a6a497ed6c8a52d6a23584
Tekere <96.sumimoto@gmail.com>
2021-06-12 10:47:51 +0000

    2021-06-12

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+require 'capybara/rspec'
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -39,6 +42,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+  
   config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
@@ -54,10 +58,16 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+  # 
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
+  # 
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless    
+  end
 end
